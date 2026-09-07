ab67687a86e07cfb766926814b902d54519e931b
willianjesusdasilva <willian140897@gmail.com>
2026-09-06 23:48:50 -0300

    Add bitmap status punctuation glyphs

--- graphics/sdl.zig
+++ graphics/sdl.zig
@@ -628,6 +628,9 @@ pub fn glyph3x5(character: u8) [5]u8 {
         ']' => .{ 6, 2, 2, 2, 6 },
         ',' => .{ 0, 0, 0, 2, 4 },
         '@' => .{ 7, 5, 7, 4, 7 },
+        '!' => .{ 2, 2, 2, 0, 2 },
+        '?' => .{ 7, 1, 2, 0, 2 },
+        '=' => .{ 0, 7, 0, 7, 0 },
         '>' => .{ 4, 2, 1, 2, 4 },
         else => .{ 7, 1, 2, 0, 2 },
     };
@@ -641,6 +644,8 @@ fn testApplicationDraw(window: *Window) void {
 test "SDL software event queue and surface contract" {
     try @import("std").testing.expectEqual([5]u8{ 0, 2, 0, 2, 0 }, glyph3x5(':'));
     try @import("std").testing.expectEqual([5]u8{ 1, 1, 2, 4, 4 }, glyph3x5('/'));
+    try @import("std").testing.expectEqual([5]u8{ 2, 2, 2, 0, 2 }, glyph3x5('!'));
+    try @import("std").testing.expectEqual([5]u8{ 0, 7, 0, 7, 0 }, glyph3x5('='));
     try @import("std").testing.expect(glyph3x5('(')[0] != glyph3x5(')')[0]);
     try @import("std").testing.expectEqual(@as(u32, 0x112233), blendRgbaOverRgb(0x112233ff, 0xaabbcc));
     try @import("std").testing.expectEqual(@as(u32, 0xaabbcc), blendRgbaOverRgb(0x11223300, 0xaabbcc));
