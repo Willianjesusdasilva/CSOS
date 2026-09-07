6d60155cdd1f03c7b919a2623ede9f3b5acc2bf0
willianjesusdasilva <willian140897@gmail.com>
2026-09-07 05:11:57 -0300

    Accept RUSAGE_CHILDREN identifier

--- kernel/syscalls.zig
+++ kernel/syscalls.zig
@@ -3243,11 +3243,12 @@ fn getRlimit(resource: u64, output: u64) u64 {
 }
 
 fn getRusage(who: u64, output: u64) u64 {
-    if (who > 1 or !validUserSlice(output, 144)) return errno(22);
+    const children = std.math.maxInt(u64);
+    if (who != 0 and who != 1 and who != children or !validUserSlice(output, 144)) return errno(22);
     const bytes: [*]u8 = @ptrFromInt(output);
     @memset(bytes[0..144], 0);
-    const seconds = monotonic_time_ns / 1_000_000_000;
-    const micros = (monotonic_time_ns % 1_000_000_000) / 1_000;
+    const seconds = if (who == children) 0 else monotonic_time_ns / 1_000_000_000;
+    const micros = if (who == children) 0 else (monotonic_time_ns % 1_000_000_000) / 1_000;
     put64(bytes, seconds);
     put64(bytes + 8, micros);
     return 0;
