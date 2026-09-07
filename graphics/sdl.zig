999884f75d7284fa91a6c4dfa702bec140b56531
willianjesusdasilva <willian140897@gmail.com>
2026-09-07 11:40:19 -0300

    Test compatible SDL audio reconfiguration

--- graphics/sdl.zig
+++ graphics/sdl.zig
@@ -1231,6 +1231,7 @@ test "SDL software event queue and surface contract" {
     try @import("std").testing.expectEqual(@as(u64, 0), audio.availableFrames());
     try @import("std").testing.expectEqual(@as(u64, 0), audio.consume(64));
     try @import("std").testing.expectEqual(@as(u64, 192), audio.queuedFrames());
+    try audio.reconfigure(.{ .sample_rate = 48000, .channels = 2 });
     try @import("std").testing.expectError(error.AudioQueued, audio.reconfigure(.{ .sample_rate = 44100, .channels = 2 }));
     try @import("std").testing.expectEqual(@as(u64, 0), audio.drain());
     try @import("std").testing.expect(audio.pause(false));
