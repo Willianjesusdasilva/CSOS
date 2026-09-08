c0422747993e3812721ed225ec438cca41d7bb5f
willianjesusdasilva <willian140897@gmail.com>
2026-09-08 11:13:54 -0300

    Send userspace surface present frame

--- kernel/main.zig
+++ kernel/main.zig
@@ -594,6 +594,8 @@ pub fn start(info: BootInfo) noreturn {
     if (ui_boot_frame_len < 2 or ui_boot_frame[0] != 1) panic("userspace UI IPC hello invalid");
     const ui_create_len = syscalls.receiveUiBootFrame(&ui_boot_frame) orelse panic("userspace UI create request missing");
     if (ui_create_len < 7 or ui_boot_frame[0] != 2) panic("userspace UI create request invalid");
+    const ui_present_len = syscalls.receiveUiBootFrame(&ui_boot_frame) orelse panic("userspace UI present request missing");
+    if (ui_present_len != 22 or ui_boot_frame[0] != 1) panic("userspace UI present request invalid");
     serial.write("userspace file-backed UI runtime ready\n");
     if (build_options.radv_runtime) {
         vfs.validateRuntimeLibrariesSelfTest() catch panic("VFS RADV runtime files self-test failed");
--- userspace/ui_runtime.zig
+++ userspace/ui_runtime.zig
@@ -1,6 +1,7 @@
 pub export const ready_message: [30]u8 = "CSOS userspace UI files ready\n".*;
 pub export const hello_frame = [_]u8{ 1, 21 } ++ "UI_HELLO file-backed\n";
 pub export const create_frame = [_]u8{ 2, 19, 0x80, 0x02, 0xe0, 0x01, 12 } ++ "HTML DESKTOP";
+pub export const present_frame = [_]u8{ 1, 22, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x80, 0x02, 0xe0, 0x01 };
 pub export const manifest_path = "/system/ui/interface/desktop.manifest\x00";
 pub export const html_path = "/system/ui/interface/desktop.html\x00";
 pub export const css_path = "/system/ui/styles/desktop.css\x00";
@@ -46,6 +47,11 @@ pub export fn _start() callconv(.naked) noreturn {
         \\mov $1, %%edi
         \\lea create_frame(%%rip), %%rsi
         \\mov $19, %%edx
+        \\syscall
+        \\mov $451, %%eax
+        \\mov $1, %%edi
+        \\lea present_frame(%%rip), %%rsi
+        \\mov $22, %%edx
         \\syscall
         \\mov $1, %%eax
         \\mov $1, %%edi
