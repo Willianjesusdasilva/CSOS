7d7ac3b35d332df297bc6a47a6fc241af52df1ee
willianjesusdasilva <willian140897@gmail.com>
2026-09-13 03:04:46 -0300

    nix: add flake dependency alias

--- kernel/vfs.zig
+++ kernel/vfs.zig
@@ -1304,6 +1304,7 @@ fn runtimeLibraryFatAlias(path: []const u8) ?[11]u8 {
     if (equal(name, "libnixexpr.so")) return "NIXEXPR SO ".*;
     if (equal(name, "libnixcmd.so")) return "NIXCMD  SO ".*;
     if (equal(name, "libnixfetchers.so")) return "NIXFETCHSO ".*;
+    if (equal(name, "libnixflake.so")) return "NIXFLAKSO  ".*;
     if (std.mem.startsWith(u8, name, "libWPEWebKit-2.0.so")) return "WEBKIT  SO1".*;
     if (std.mem.startsWith(u8, name, "libWPEBackend-fdo-1.0.so")) return "WPEFDO  SO1".*;
     return null;
