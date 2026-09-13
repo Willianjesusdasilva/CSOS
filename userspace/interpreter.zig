ff78944117baef3fa3c041149e2e89587c41fac8
willianjesusdasilva <willian140897@gmail.com>
2026-09-13 05:07:28 -0300

    userspace: preserve constructor registers

--- userspace/interpreter.zig
+++ userspace/interpreter.zig
@@ -1,6 +1,4 @@
 pub export const message: [29]u8 = "Linux PT_INTERP loader ready\n".*;
-pub export const bootstrap_message: [24]u8 = "musl bootstrap returned\n".*;
-pub export const constructors_message: [19]u8 = "constructors begin\n".*;
 
 pub export fn _start() callconv(.naked) noreturn {
     asm volatile (
@@ -55,21 +53,11 @@ pub export fn _start() callconv(.naked) noreturn {
         \\call *%%rax
         \\testl %%eax, %%eax
         \\jnz 9f
-        \\movq $1, %%rax
-        \\movq $1, %%rdi
-        \\leaq bootstrap_message(%%rip), %%rsi
-        \\movq $24, %%rdx
-        \\syscall
         \\12:
         \\testq %%r15, %%r15
         \\jz 8f
         \\testq %%r14, %%r14
         \\jz 9f
-        \\movq $1, %%rax
-        \\movq $1, %%rdi
-        \\leaq constructors_message(%%rip), %%rsi
-        \\movq $19, %%rdx
-        \\syscall
         \\xorq %%rbx, %%rbx
         \\10: movq (%%r12), %%rdi
         \\leaq 8(%%r12), %%rsi
