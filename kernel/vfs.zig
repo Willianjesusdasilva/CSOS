6ca32d2e4abb28be641fe8dbf5148d9534b68a45
willianjesusdasilva <willian140897@gmail.com>
2026-09-13 04:07:38 -0300

    nix: alias pcre2 runtime library

--- kernel/vfs.zig
+++ kernel/vfs.zig
@@ -1342,6 +1342,7 @@ fn runtimeLibraryFatAlias(path: []const u8) ?[11]u8 {
     if (equal(name, "libpsl.so.5")) return "LIBPSL  SO5".*;
     if (equal(name, "libssl.so.3")) return "LIBSSL  SO3".*;
     if (equal(name, "libllhttp.so.9.4")) return "LLHTTP  SO9".*;
+    if (equal(name, "libpcre2-8.so.0")) return "PCRE2   SO0".*;
     if (std.mem.startsWith(u8, name, "libWPEWebKit-2.0.so")) return "WEBKIT  SO1".*;
     if (std.mem.startsWith(u8, name, "libWPEBackend-fdo-1.0.so")) return "WPEFDO  SO1".*;
     return null;
--- tools/make-nix-fat16.ps1
+++ tools/make-nix-fat16.ps1
@@ -47,6 +47,7 @@ $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libidn2.so.0 ::/lib/LIBID
 $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libpsl.so.5 ::/lib/LIBPSL.SO5"
 $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libssl.so.3 ::/lib/LIBSSL.SO3"
 $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libllhttp.so.9.4 ::/lib/LLHTTP.SO9"
+$command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libpcre2-8.so.0 ::/lib/PCRE2.SO0"
 $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libz.so.1 ::/lib/LIBZ.SO1"
 $encoded = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($command))
 & wsl.exe -d Ubuntu -- bash -lc "echo $encoded | base64 -d | bash"
