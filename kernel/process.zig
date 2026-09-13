e13ccfae821be56d62c2938f40bde836cbbc6a88
willianjesusdasilva <willian140897@gmail.com>
2026-09-13 04:26:00 -0300

    process: identify failing shared initializers

--- kernel/process.zig
+++ kernel/process.zig
@@ -609,6 +609,9 @@ fn runImageWithWorkspace(
         while (provider_initializer_index > 0) {
             provider_initializer_index -= 1;
             const provider = providers[provider_initializer_index];
+            serial.write("collecting shared initializer ");
+            serial.writeDecimal(provider_initializer_index);
+            serial.write("\n");
             try collectInitializers(provider.bytes, provider.program_offset, provider.program_entry_size, provider.program_count, provider.base, mappings[0..mapping_count.*], &initializers, &initializer_count);
         }
         try collectInitializers(program_image, program_offset, program_entry_size, program_count, load_bias, mappings[0..mapping_count.*], &initializers, &initializer_count);
