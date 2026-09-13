ba214f24da82b9fc607d11bbed7d226d2e5a1f51
willianjesusdasilva <willian140897@gmail.com>
2026-09-13 07:03:49 -0300

    vfs: expose persistent system directories

--- kernel/vfs.zig
+++ kernel/vfs.zig
@@ -13,6 +13,10 @@ const Node = enum {
     proc_self,
     proc_exe,
     proc_stat,
+    home,
+    data,
+    system,
+    tmp,
     bin,
     dev,
     dri,
@@ -1148,6 +1152,10 @@ fn resolve(directory_fd: i64, path: []const u8) !Node {
     if (equal(path, "/proc/self")) return .proc_self;
     if (equal(path, "/proc/self/exe") or equal(path, "proc/self/exe")) return .proc_exe;
     if (equal(path, "/proc/stat")) return .proc_stat;
+    if (equal(path, "/home")) return .home;
+    if (equal(path, "/data")) return .data;
+    if (equal(path, "/system")) return .system;
+    if (equal(path, "/tmp")) return .tmp;
     if (equal(path, "/bin") or equal(path, "bin")) return .bin;
     if (equal(path, "/dev") or equal(path, "dev")) return .dev;
     if (equal(path, "/dev/null") or equal(path, "dev/null")) return .null_device;
@@ -1182,7 +1190,7 @@ fn resolve(directory_fd: i64, path: []const u8) !Node {
 
 fn nodeInfo(node: Node) Info {
     return switch (node) {
-        .root, .bin, .dev, .dri, .sys, .sys_dev, .sys_char, .proc, .proc_self, .drm_char_primary, .drm_char_render, .drm_device, .drm_device_drm, .fat_directory => .{ .mode = 0o040755, .size = 0, .directory = true },
+        .root, .bin, .dev, .dri, .sys, .sys_dev, .sys_char, .proc, .proc_self, .home, .data, .system, .tmp, .drm_char_primary, .drm_char_render, .drm_device, .drm_device_drm, .fat_directory => .{ .mode = 0o040755, .size = 0, .directory = true },
         .proc_stat => .{ .mode = 0o100444, .size = "cpu 0 0 0 0 0 0 0 0 0 0\n\n".len, .directory = false },
         .proc_exe => .{ .mode = 0o120777, .size = "/nix/bin/nix".len, .directory = false },
         .busybox => .{ .mode = 0o100755, .size = busybox.len, .directory = false },
