b45a696b01b13090f7659d63fbe5b8540ffaffc6
willianjesusdasilva <willian140897@gmail.com>
2026-09-13 03:24:19 -0300

    nix: alias crypto runtime library

--- kernel/vfs.zig
+++ kernel/vfs.zig
@@ -1312,6 +1312,7 @@ fn runtimeLibraryFatAlias(path: []const u8) ?[11]u8 {
     if (equal(name, "libc.musl-x86_64.so.1")) return "LIBCMUSLSO1".*;
     if (equal(name, "libarchive.so.13")) return "LIBARCH SO3".*;
     if (equal(name, "libblake3.so.0")) return "LIBBLAK SO0".*;
+    if (equal(name, "libcrypto.so.3")) return "LIBCRYP SO3".*;
     if (std.mem.startsWith(u8, name, "libWPEWebKit-2.0.so")) return "WEBKIT  SO1".*;
     if (std.mem.startsWith(u8, name, "libWPEBackend-fdo-1.0.so")) return "WPEFDO  SO1".*;
     return null;
