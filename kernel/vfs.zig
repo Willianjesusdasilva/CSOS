ca6868dd32073816db6601c83dd53b0b557ac636
willianjesusdasilva <willian140897@gmail.com>
2026-09-13 03:41:16 -0300

    nix: alias acl runtime library

--- kernel/vfs.zig
+++ kernel/vfs.zig
@@ -1328,6 +1328,7 @@ fn runtimeLibraryFatAlias(path: []const u8) ?[11]u8 {
     if (equal(name, "libgit2.so.1.9")) return "LIBGIT2 SO1".*;
     if (equal(name, "liblowdown.so.4")) return "LIBLOWD SO4".*;
     if (equal(name, "libeditline.so.1")) return "LIBEDIT SO1".*;
+    if (equal(name, "libacl.so.1")) return "LIBACL  SO1".*;
     if (std.mem.startsWith(u8, name, "libWPEWebKit-2.0.so")) return "WEBKIT  SO1".*;
     if (std.mem.startsWith(u8, name, "libWPEBackend-fdo-1.0.so")) return "WPEFDO  SO1".*;
     return null;
