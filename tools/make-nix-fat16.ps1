9cc2bd3beeff18e9ad068bd54d86fe329d505e72
willianjesusdasilva <willian140897@gmail.com>
2026-09-13 03:47:13 -0300

    nix: package z runtime alias

--- tools/make-nix-fat16.ps1
+++ tools/make-nix-fat16.ps1
@@ -38,6 +38,7 @@ $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libexpat.so.1 ::/lib/LIBE
 $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libzstd.so.1 ::/lib/LIBZSTD.SO1"
 $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/liblz4.so.1 ::/lib/LIBLZ4.SO1"
 $command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/liblzma.so.5 ::/lib/LIBLZMA.SO5"
+$command += "; mcopy -o -i '$outputWsl' `$root/usr/lib/libz.so.1 ::/lib/LIBZ.SO1"
 $encoded = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($command))
 & wsl.exe -d Ubuntu -- bash -lc "echo $encoded | base64 -d | bash"
 if ($LASTEXITCODE -ne 0) { throw "Nix FAT image creation failed ($LASTEXITCODE)" }
